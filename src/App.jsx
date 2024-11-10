97405c6a2301e00f3fd210729a072f85235e3ebe
Shahadat Hussain Ripon <developershahadathussainripon@gmail.com>
2024-11-11 00:28:12 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <div className='flex justify-center items-center h-screen w-full'>
+            <h1 className='text-teal-800 text-4xl'>
+                Templete with React and Tailwindcss
+            </h1>
+        </div>
+    );
+};
+
+export default App;
+
